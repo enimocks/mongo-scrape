1403e95ba745f7ecdd208d6e66c8a54773111f33
Edward Nimocks <enimocks@gmail.com>
2018-06-27 14:45:59 -0400

    models done

--- models/article.js
+++ models/article.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var ArticlesSchema = new Schema({
+var ArticleSchema = new Schema({
   headline: {
     type: String,
     unique: true,
